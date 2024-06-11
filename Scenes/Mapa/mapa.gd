64f90c2c4bbc0107ed8093da3560991e6bf16998
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-10 22:31:37 -0400

    Conflict with map files solved

--- Scenes/Mapa/mapa.gd
+++ Scenes/Mapa/mapa.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 @onready var label_3d = $Sprite3D
-<<<<<<< HEAD
 @onready var audio_stream_player = $AudioStreamPlayer
 
 func _on_timer_timeout():
@@ -19,8 +18,3 @@ func _on_area_3d_body_exited(body):
 	if "player" in str(body):
 		print("ALGO salio AL AREA")
 		audio_stream_player.stop()
-=======
-
-func _on_timer_timeout():
-	pass # Replace with function body.
->>>>>>> 66636fafee9ff6a924e63b41d5c289407ac97aa2
