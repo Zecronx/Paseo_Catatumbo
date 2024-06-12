8ca63ec28b380145abd7f379bdfab0f7609ee75e
Zecronx <reyesferrerjose@gmail.com>
2024-06-11 23:13:19 -0400

    Agregado techo 1er nivel

--- Assets/Marco/marco.gd
+++ Assets/Marco/marco.gd
@@ -8,23 +8,26 @@ extends StaticBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite_3d.texture = imagen
+	if imagen != null:
+		sprite_3d.texture = imagen
+		
 	if audio != null:
 		audio_stream_player.stream = audio
 	
 func _on_area_3d_body_entered(body):
 	if "player" in str(body):
-			timer_entered.start()
+		timer_entered.start()
 
 func _on_area_3d_body_exited(body):
 	#timerend.start
 	if "player" in str(body):
 		timer_exited.start()
 		
-func _on_timer_timeout():
-	audio_stream_player.play()
-	pass # Replace with function body.
-
 func _on_timer_exited_timeout():
 	audio_stream_player.stop()
+	pass # Replace with function body.
+
+
+func _on_timer_entered_timeout():
+	audio_stream_player.play()
 	pass # Replace with function body.
