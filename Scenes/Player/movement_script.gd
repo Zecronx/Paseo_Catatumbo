15fb2d59ba5406249207047e5a77ddce50f9e94b
Alepex18 <ale.arteaga.18.09@gmail.com>
2024-07-18 14:27:50 -0400

    Spawn cambiado al inicio
    Si le dan a la tecla de backspace, mueve el personaje al museo

--- Scenes/Player/movement_script.gd
+++ Scenes/Player/movement_script.gd
@@ -87,7 +87,9 @@ func _physics_process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		elif Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
+	if Input.is_key_pressed(KEY_BACKSPACE):
+		position = Vector3(0,0,0)
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
